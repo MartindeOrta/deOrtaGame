adfe8b3bb26531e65bca8b7123d92d443b693673
MartindeOrta <82926690+MartindeOrta@users.noreply.github.com>
2025-01-24 09:36:28 -0300

    Update scriptDatos.js

--- scriptDatos.js
+++ scriptDatos.js
@@ -1,6 +1,6 @@
 
-import { Abi } from 'https://martindeorta.github.io/MartyrNFT/Abi.js';
-import { MartyrNFT } from 'https://martindeorta.github.io/MartyrNFT/ContractAddress.js';
+import { Abi } from 'https://martindeorta.github.io/deOrtaGame/MartyrNFT/Abi.js';
+import { MartyrNFT } from 'https://martindeorta.github.io/deOrtaGame/MartyrNFT/ContractAddress.js';
 var abi= Abi;
 var martyrNFT= MartyrNFT;
 console.log(Abi)
